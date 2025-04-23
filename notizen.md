b0866e465df990f29ff0c9c0faed3f8978c7ea5b
furukawa <dkszoeke@outlook.de>
2025-04-23 22:53:15 +0200

    implemented client log in
    set permissions for the bot, installed nodemon, then programmed intents and pasted into console. Lots of stuff to learn: how do JavaScript callbacks work, wtf is nodemon etc.

---
+++ notizen.md
@@ -0,0 +1,11 @@
+1. Create nodejs project
+2. Add Discord Bot
+    * Auth=application.commands; bot
+    * BotPermissions=View Channels, and all Text Permissions but Send TTS Messages
+    * ServerSettings/Roles/Permissions/ManageServer
+    * Toggle on *Server Members Intent* and *Message Content Intent*
+3. Install nodemon, to automatically restart bot
+4. Add Intents for discord.js
+
+Figure out what intents are.
+Figure out how callback functions work => {}
--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
-const { Client, IntensBitField } = require{'discord.js'};
-const client = new Client()
+require("dotenv").config();
+const { Client, IntentsBitField } = require("discord.js");
+
+// setup client, then specify which intents the client uses (they are necessary) 
+const client = new Client({
+	intents: [
+		IntentsBitField.Flags.Guilds,
+		IntentsBitField.Flags.GuildMembers,
+		IntentsBitField.Flags.GuildMessages,
+		IntentsBitField.Flags.MessageContent,
+	],
+});
+
+// specify which events Discord will send to the bot using intents
+
+client.on("ready", () => {
+	console.log(`Logged in as ${client.user.tag}`)
+})
+
+
+// login with the above settings
+client.login(process.env.TOKEN)

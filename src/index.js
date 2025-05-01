9b1c6d2d87f986c4c03948b0883e6a3ec975bfda
furukawa <dkszoeke@outlook.de>
2025-05-01 20:27:57 +0200

    added weather api from open-meteo
    I used async function for the first time, to wait for the URL to be fetched, or not. But still have to figure out async functions. Can't call an async inside a normal function, why?

--- src/index.js
+++ src/index.js
@@ -11,12 +11,52 @@ const client = new Client({
 	],
 });
 
-// specify which events Discord will send to the bot using intents
+// upon "Hey", write "Spice is the essence of life."
+const spice = (client) => {
+	client.on("messageCreate", msg => {
+		if (msg.author.bot) return;
 
-client.on("ready", () => {
-	console.log(`Logged in as ${client.user.tag}`)
-})
+		if (msg.content === "Hey!") {
+			msg.reply("Spice is the essence of life.");
+		}
+	});
+};
+
+// put location here
+	// hannover
 
 
-// login with the above settings
+
+// post what the weather will be like this week
+const lat = 52.38;
+const lon = 9.74;
+
+const getWeather = async () => {
+	const apiURL = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m`;
+
+	try {
+		const res = await fetch(apiURL);
+		if (!res.ok) {
+			throw new Error('Network response failed');
+		}
+		const data = await res.json();
+		console.log('Weather data', data);
+		return data;
+	} catch (error) {
+		console.error('Error fetching weather data:', error);
+	}
+};
+
+
+
+client.on("ready", async () => {
+	console.log(`Logged in as ${client.user.tag}`);
+	spice(client);
+
+	const weather = await getWeather();
+	console.log(weather);
+});
+
+
+// login bot
 client.login(process.env.TOKEN)

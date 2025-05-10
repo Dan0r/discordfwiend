640bac3b9a67585bd1fbe8307315d07d89e16c38
furukawa <dkszoeke@outlook.de>
2025-05-10 22:58:25 +0200

    Translated weathercodes
    Took weathercodes and translated them into strings, with which users can understand the weather by

--- src/index.js
+++ src/index.js
@@ -26,6 +26,34 @@ const spice = (client) => {
 	// hannover
 
 
+// Translate weather_code
+const weather_code_map = new Map([
+	[0, "Clear sky"],
+	[1, "Mainly clear"],
+	[2, "Partly cloudy"],
+	[3, "Overcast"],
+	[45, "Fog"],
+	[48, "Fog: Depositing rime "],
+	[51, "Drizzle: Light"],
+	[53, "Drizzle: Moderate"],
+	[55, "Drizzle: Dense"],
+	[56, "Drizzle: Light freezing"],
+	[57, "Drizzle: Dense freezing"],
+	[66, "Rain: Light freezing "],
+	[67, "Rain: Heavy freezing "],
+	[71, "Snow: Slight"],
+	[73, "Snow: Moderate"],
+	[75, "Snow: Heavy"],
+	[77, "Snow: Grains"],
+	[80, "Rain: Slight"],
+	[81, "Rain: Moderate"],
+	[82, "Rain: Violent"],
+	[85, "Snow Shower: Slight"],
+	[86, "Snow Shower: Heavy"],
+	[95, "Thunderstorm: Slight"],
+	[96, "Thunderstorm: With slight hail"],
+	[99, "Thunderstorm: With heavy hail"],
+]);
 
 // post what the weather will be like this week (for Hannover)
 const getWeather = async () => {
@@ -33,6 +61,7 @@ const getWeather = async () => {
 	const lat = 52.38;
 	const lon = 9.74;
 	const timezone = "Europe/Berlin"
+
 
 
 	// fetching data from open Meteo
@@ -50,16 +79,23 @@ const getWeather = async () => {
 		// data from Germany is from the DWD, that updates every 3 hours
 		// fetch tomorrows temperature (current day = 24, tomorrow at 12 = 24 + 12)
 		const { temperature_2m, weather_code: current_weather_code } = data.current;
-		const current_temperature = temperature_2m
+		const current_temperature = temperature_2m;
 
 		const { temperature_2m_max, weather_code: daily_weather_code } = data.daily;
-		const tomorrows_temperature_max = temperature_2m_max[1]
-		const tomorrows_weather = daily_weather_code[1]
-		const todays_weather= daily_weather_code[0]
+		const todays_temperature_max = temperature_2m_max[0];
+		const todays_weather = weather_code_map.get(daily_weather_code[0]);
+		const tomorrows_temperature_max = temperature_2m_max[1];
+		const tomorrows_weather = weather_code_map.get(daily_weather_code[1]);
 
 
 
-		return { current_temperature, todays_weather, tomorrows_temperature_max, tomorrows_weather};
+		return {
+		  current_temperature,
+		  todays_temperature_max,
+		  todays_weather,
+		  tomorrows_temperature_max,
+		  tomorrows_weather
+		};
 
 	} catch (error) {
 		console.error('Error fetching weather data:', error);
@@ -77,13 +113,16 @@ const callback = (getWeatherCallback) => {
 
 		// only accept now and tomorrow input
 		const input = msg.content.toLowerCase();
-		if (input !== "today" && input !== "tomorrow") return; 
+		if (!["today", "tomorrow"].includes(input)) return; 
 
 		// send data
 		if (input === "today") {
-			msg.reply(`${weather.current_temperature}`);
-		} else {
-			msg.reply(`${weather.tomorrow_temperature}`)
+			msg.reply(`Now: ${weather.current_temperature}°C. Maximum: ${weather.todays_temperature_max}°C`);
+		} else if (input === "tomorrow")  {
+			msg.reply(`Maximum: ${weather.tomorrows_temperature_max}°C`);
+		}
+		else {
+			msg.reply("Invalid Input");
 		}
 	});
 };
